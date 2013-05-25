8be4df644df015f73d66dc22729a45a65c6b8ef8
Paul Killoran <paul@exordo.com>
2013-05-25 17:22:56 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestProject::Application.config.secret_token = 'f0a6aeeb305d524fe10e47466d4000b91b0bce44882f9d6aeb4823d5c608fb9414dd0708bbed62c21307c2a3c500147b157ab2959c28da4c2b70cc5d51b9b344'
